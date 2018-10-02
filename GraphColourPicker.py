a0e6125ed9e82b5c41ef972df69e48ee11e54da0
ironcore1 <ironcore1@gmail.com>
2018-10-02 21:14:28 +0100

    Checking Nodes
    Made a start at looking at trying to grab the correct node...so far no luck but I'm sure I will get it.

--- GraphColourPicker.py
+++ GraphColourPicker.py
@@ -6,25 +6,33 @@ allScene = cmds.ls()
 #all objects selected, check relatives to see if there is a SHAPE and place them in the allShapes varible
 allShapes = cmds.listRelatives(allScene, shapes=True)
 
-#grab all Nurbs in the shapes 
+#grab all Nurbs in the shapes
 nurbs= cmds.ls(allShapes, type="nurbsCurve")
 
+#this varible will hold all of the duplicate transforms
 nurbsTransformDuplicateList = []
 
 # go through nurbs variable and interate and add to the nurbsTransformDuplicateList array
 for shape in nurbs:
-    nurbsTransformDuplicateList.append(cmds.listRelatives(shape, parent=True)[0]) 
+    nurbsTransformDuplicateList.append(cmds.listRelatives(shape, parent=True)[0])
 
 
 #quickly see how many items are in the nurbsTransformDuplicateList varible
 print len(nurbsTransformDuplicateList)
 
-#remove duplicates    
+#remove duplicates
 nurbsTransList = set(nurbsTransformDuplicateList)
-    
-#derp = cmds.listAttr(nurbsTransformParent,visible=True, keyable=True, scalar=True, unlocked=True)
-#   print derp
 
-thisIsSet={1,2,3,4,5,2,3,4}
 
-print thisIsSet
+#transformAttr = cmds.listAttr('CtrlRig:AN_IK_R_Hand_Control',visible=True, keyable=True, scalar=True, unlocked=True)
+
+for i in nurbsTransList:
+
+    transformAttr = cmds.listAttr(i,visible=True, keyable=True, scalar=True, unlocked=True)
+    #grab the name of the object currently selected and list all connecting within the node editor
+    connections = cmds.listConnections(i, plugs=True,s=True)
+
+
+
+
+
