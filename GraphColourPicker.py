5d1126259ae362cdefe436e6540530f688e486e1
ironcore1 <ironcore1@gmail.com>
2018-10-12 16:29:43 +0100

    Nurbs Grabber
    Started making a crack at trying to grab the nurbsCurveTU

--- GraphColourPicker.py
+++ GraphColourPicker.py
@@ -1,5 +1,15 @@
 import maya.cmds as cmds
 
+rotateAtt = ['RotateX', 'RotateY', 'RotateZ']
+transAtt = ['TranslateX', 'TranslateY', 'TranslateZ']
+scaleAtt = ['ScaleX', 'ScaleY', 'ScaleZ']
+
+removeTranslate = False
+removeRotate = False
+removeScale = False 
+
+value = 0
+
 #grab everything within Maya
 allScene = cmds.ls()
 
@@ -24,13 +34,45 @@ print len(nurbsTransformDuplicateList)
 nurbsTransList = set(nurbsTransformDuplicateList)
 
 
-#transformAttr = cmds.listAttr('CtrlRig:AN_IK_R_Hand_Control',visible=True, keyable=True, scalar=True, unlocked=True)
-
 for i in nurbsTransList:
 
+    #get all attribute which are visible to the user and are also keyable
     transformAttr = cmds.listAttr(i,visible=True, keyable=True, scalar=True, unlocked=True)
-    #grab the name of the object currently selected and list all connecting within the node editor
-    connections = cmds.listConnections(i, plugs=True,s=True)
+    print transformAttr
+    
+    #store all source nurb connections in connectionNurb
+    connectionNurb =[]
+    lengthAttr = len(transformAttr)
+    
+    #grab the name of the object currently selected and list all connecting within the node editor and be sure to only grab the source
+    
+    #while value != lengthAttr:
+        #connectionNurb.append(cmds.listConnections(lengthAttr[value],source=True, type='animCurveTA'))
+        #value = value + 1
+    
+   
+'''    
+   
+    #connectionNurb.append(cmds.listConnections(i,source=True, type='animCurveTL' or 'animCurveTU' or 'animCurveTA')) 
+
+    
+   
+    if connectionNurb[0] is not None:
+        
+    
+        for x in connectionNurb[0]:
+            print x
+            
+            cmds.setAttr(x+".useCurveColor",1)
+            
+            attributeName = cmds.listConnections(x,plugs=True)
+            
+            
+            
+            if '.rotate' in attributeName[0]:
+                print attributeName
+            
+      '''      
 
 
 
